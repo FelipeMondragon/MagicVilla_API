91c45b001e67ee36ed9f585623b7f93f3f4bacb5
FelipeMondragon <luis.felipe.mondragon@proton.me>
2023-08-03 12:12:36 -0600

    IndexVilla and CreateVilla view and Villa Service

---
+++ MagicVilla_Web/Controllers/VillaController.cs
@@ -0,0 +1,54 @@
+﻿using AutoMapper;
+using MagicVilla_Web.DTOs;
+using MagicVilla_Web.IServices;
+using MagicVilla_Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace MagicVilla_Web.Controllers
+{
+    public class VillaController : Controller
+    {
+        private readonly IVillaService _villaService;
+        private readonly IMapper _mapper;
+
+        public VillaController(IVillaService villaService, IMapper mapper)
+        {
+            _villaService = villaService;
+            _mapper = mapper;
+        }
+
+        public async Task<IActionResult> IndexVilla()
+        {
+            List<VillaDTO> list = new();
+
+            var response = await _villaService.GetAllAsync<APIResponse>();  
+            if(response != null && response.IsSucces)
+            {
+                list = JsonConvert.DeserializeObject<List<VillaDTO>>(Convert.ToString(response.Result));
+            }
+            return View(list);
+        }
+        
+        public async Task<IActionResult> CreateVilla()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CreateVilla(VillaCreateDTO createDTO)
+        {
+            if (ModelState.IsValid)
+            {
+                var response = await _villaService.CreateAsync<APIResponse>(createDTO);
+                if (response != null && response.IsSucces)
+                {
+                    return RedirectToAction(nameof(IndexVilla));
+                }
+            }
+            return View(createDTO);
+        }
+    }
+}
---
+++ MagicVilla_Web/Services/VillaService.cs
@@ -0,0 +1,66 @@
+﻿using MagicVilla_Utility;
+using MagicVilla_Web.DTOs;
+using MagicVilla_Web.IServices;
+using MagicVilla_Web.Models;
+
+namespace MagicVilla_Web.Services
+{
+    public class VillaService : BaseService, IVillaService
+    {
+        private readonly IHttpClientFactory _clientFactory;
+        private string _villaUrl;
+
+        public VillaService(IHttpClientFactory clientFactory, IConfiguration configuration) : base(clientFactory) 
+        {
+            _clientFactory = clientFactory;
+            _villaUrl = configuration.GetValue<string>("ServiceUrls:VillaAPI");
+        }
+
+        public Task<T> CreateAsync<T>(VillaCreateDTO dto)
+        {
+            return SendAsync<T>(new APIRequest()
+            {
+                ApiType = SD.ApiType.POST,
+                URL = _villaUrl + "/api/villaAPI",
+                Data = dto
+            });
+        }
+
+        public Task<T> DeleteAsync<T>(int id)
+        {
+            return SendAsync<T>(new APIRequest()
+            {
+                ApiType = SD.ApiType.DELETE,
+                URL = _villaUrl + "/api/villaAPI/" + id,
+            });
+        }
+
+        public Task<T> GetAllAsync<T>()
+        {
+            return SendAsync<T>(new APIRequest()
+            {
+                ApiType = SD.ApiType.GET,
+                URL = _villaUrl + "/api/villaAPI",
+            });
+        }
+
+        public Task<T> GetAsync<T>(int id)
+        {
+            return SendAsync<T>(new APIRequest() 
+            { 
+                ApiType = SD.ApiType.GET, 
+                URL = _villaUrl + "/api/villaAPI/" + id 
+            });
+        }
+
+        public Task<T> UpdateAsync<T>(VillaUpdateDTO dto)
+        {
+            return SendAsync<T>(new APIRequest() 
+            { 
+                ApiType = SD.ApiType.PUT, 
+                URL = _villaUrl + "/api/villaAPI/" + dto.Id,
+                Data = dto
+            });
+        }
+    }
+}
